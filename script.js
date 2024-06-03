ae5f67ec3be4ee6e88abc08619ee15fa5ed76493
Ahmedgaigo <ahmetgaigo68@gmail.com>
2024-06-03 17:51:52 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 document.getElementById('theme').addEventListener('click', function(event) {
     event.preventDefault();  // Prevent the default anchor behavior
     document.body.classList.toggle('dark-mode');
-});
+    
+    // Toggle dark mode for nav items
+    let navItems = document.querySelectorAll('.nav_item');
+    navItems.forEach(function(item) {
+        item.classList.toggle('dark-mode');
+    });
 
+    // Toggle dark mode for <i> items inside nav items
+    let navIcons = document.querySelectorAll('.nav_item i');
+    navIcons.forEach(function(icon) {
+        icon.classList.toggle('dark-mode-icon');
+    });
+});
